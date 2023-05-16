698a502632f0c61964924696900d11953c433bc5
kristavanderweg <kristavanderweg@gmail.com>
2023-05-16 10:52:59 +0200

    huiswerkopgave 3 4 en 5 toegevoegd

---
+++ tijdelijk.py
@@ -0,0 +1,10 @@
+prijzen ={
+    "aardbei" : "3",
+    "vanille" : "4",
+    "chocolade" : "5"
+}
+
+aanbieding = (3*0.8)
+
+reclame_tekst =f"Vandaag in de aanbieding: aardbei-ijs, 1 liter -slechts € {aanbieding}"
+print (reclame_tekst)
