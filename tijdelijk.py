e087ebe39f450845033f875096544ed399bc1b63
kristavanderweg <kristavanderweg@gmail.com>
2023-05-16 14:47:25 +0200

    bestand tijdelijk.py gecodeerd

--- tijdelijk.py
+++ tijdelijk.py
@@ -11,4 +11,7 @@ reclame_tekst2 = reclame_tekst [:62]
 reclame_tekst3 = reclame_tekst.upper() [:62]
 reclame_tekst4 = reclame_tekst3.split() 
 for el in reclame_tekst4:
-    print (el.lower())
+    if len ((el)) >= 5:
+        print(el.upper())
+    else:
+        print (el.lower())
