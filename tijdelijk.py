a0edaf0fe3af8d12c7618df66e8463b983ba90fa
kristavanderweg <kristavanderweg@gmail.com>
2023-05-16 11:05:25 +0200

    opdracht 5 uitgewerkt

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,10 +1,11 @@
 prijzen ={
-    "aardbei" : "3",
-    "vanille" : "4",
-    "chocolade" : "5"
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5
 }
 
 aanbieding = (3*0.8)
 
 reclame_tekst =f"Vandaag in de aanbieding: aardbei-ijs, 1 liter -slechts € {aanbieding}"
-print (reclame_tekst)
+reclame_tekst2 = reclame_tekst [:62]
+print (reclame_tekst2)
