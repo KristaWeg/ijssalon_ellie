7617d943e155ee3718cd9d19aec0546cb49fddf9
kristavanderweg <kristavanderweg@gmail.com>
2023-05-31 13:22:11 +0200

    huiswerkopgaven les 10

---
+++ presentatie.py
@@ -0,0 +1,19 @@
+mijn_dict= {
+    'vis' : 10,
+    'vlees': 25,
+    'overig': 15
+}
+
+totaal = 50
+
+def presenteer(dictionary, totaal):
+    
+    for k,v in dictionary.items():
+        print (k, ":", v, "euro")
+    print (26 * "=")
+    print ("Totaal:", totaal, "euro")
+    
+    
+
+
+print (presenteer(mijn_dict, totaal))
